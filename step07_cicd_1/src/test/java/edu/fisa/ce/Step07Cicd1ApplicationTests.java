6a63438272d1d2862303644698dedb053855da08
이은준 <eunjunlee98@naver.com>
2025-04-09 10:45:07 +0900

    update code

--- step07_cicd_1/src/test/java/edu/fisa/ce/Step07Cicd1ApplicationTests.java
+++ step07_cicd_1/src/test/java/edu/fisa/ce/Step07Cicd1ApplicationTests.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals; // JUnit 5
 class Step07CicdApplicationTests {
 	@Test
 	public void testAddition() {
-		int result = 2 + 3;
+		int result = 12 + 3;
 		/* 예시 - 서버시작시에 특정 datebase 로부터 검색
 		 * 그 데이터는 꼭 fisa여야만 하는상항
 		 * fisa가 없다면 서버 중지 또는 다른 로직실행
@@ -19,7 +19,7 @@ class Step07CicdApplicationTests {
 		 */
 		System.out.println("****비교 전****");
 
-		assertEquals(5, result); // 성공: 5 == 5
+		assertEquals(15, result); // 성공: 5 == 5
 		System.out.println("****비교 후****");
 	}
 }
